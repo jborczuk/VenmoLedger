1f7a1182a455f1a1f9091b4d692852f520b4576d
Justin Borczuk <jnb1996@gmail.com>
2016-01-16 21:01:56 -0500

    Successful Print to Text File

--- app/views.py
+++ app/views.py
@@ -12,7 +12,7 @@ import requests, os, urllib,cStringIO
 @app.route('/')
 def index():
     CONSUMER_ID = ???? #hidden for security
-    return redirect('https://api.venmo.com/v1/oauth/authorize?client_id=%s&scope=make_payments,access_profile&response_type=code' % CONSUMER_ID)
+    return redirect('https://api.venmo.com/v1/oauth/authorize?client_id=%s&scope=make_payments,access_profile&response_type=token' % CONSUMER_ID)
 
 @app.route('/oauth-authorized', methods=['GET', 'POST'])
 def login():
@@ -20,11 +20,11 @@ def login():
     if form.validate_on_submit():
         AUTHORIZATION_CODE = request.args.get('code')
         data = {
-        "client_id":CONSUMER_ID,
-        "client_secret":CONSUMER_SECRET,
+        "client_id":???,
+        "client_secret":'vXpe8uzQZWs6mAEEbSUwWz3FYP8SJaZJ',
         "code":AUTHORIZATION_CODE
         }
-
+        
         date1 = form.openid.data.split(' ')
         date2 = form.openid2.data.split(' ')
         start = date1[2] + "-" + date1[0] + "-" + date1[1]+"T00:00:00"
@@ -34,19 +34,21 @@ def login():
         response_dict = response.json()
         print(response_dict)
         #url = 'https://api.venmo.com/v1/oauth/authorize?client_id=????&scope=make_payments%20access_profile&response_type=token'
-        
+        access_token = response_dict.get('access_token')
         #print(r)
-        #url = 'https://api.venmo.com/v1/payments?access_token=3ca8f765f9b4b3ba73e9d515fce5b2d56d30ead7d8cb861747c0832327d2cf4a&date_created=' + 'start&date_completed=' + 'end'
+        url = 'https://api.venmo.com/v1/payments?access_token=49f5f5100703d53424a7e585146cb76260188edb37aab9447d472d45ecd4476f&date_created=' + 'start&date_completed=' + 'end'
+        r = requests.get(url)
+        response = r.json()
+        pprint(response)
+        f = open('ledger.txt', 'w')
         
-        #r = requests.get(url)
-        #response = r.json()
-        # pprint(response)
-        #f = open('ledger.txt', 'r+')
-        
-        # length = len(response['data'])
+        length = len(response['data'])
         #print(length)
-        # for number in range(0, length):
-        #f.write(response['data'][number]['amount'])
+        for number in range(0, length):
+            f.write("%s \n" %response['data'][number]['amount'])
+        
+
+        f.close()
        
         return redirect('/submit')
 
